a160e52f02a52b06a6ad2cb80c9d0bef207164e4
Li Donghua <donghua.li@dianping.com>
2013-12-20 22:37:55 +0800

    Add save logic for create new place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,4 +7,18 @@ class PlacesController < ApplicationController
     @place.latitude = params[:latitude]
     @place.longitude = params[:longitude]
   end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to @place, notice: 'Place was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def place_params
+    params.require(:place).permit(:latitude, :longitude, :mood, :things)
+  end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,3 @@
 class Place < ActiveRecord::Base
+  validates :latitude, :longitude, :mood, presence: true
 end
