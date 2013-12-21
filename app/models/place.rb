e4241750719b51190de44f8329300ba65e89ac67
Li Donghua <donghua.li@dianping.com>
2013-12-21 08:35:56 +0800

    Fix index creation error when already exists.

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,27 +2,29 @@ class Place < ActiveRecord::Base
   validates :latitude, :longitude, :comments, presence: true
   has_many :comments
 
-  $es.indices.create index: "shizuka", body: {
-    mappings: {
-      place: {
-        properties: {
-          location: {
-            type: "geo_shape",
-            precision: "10m"
+  unless $es.indices.exists index: "shizuka"
+    $es.indices.create index: "shizuka", body: {
+      mappings: {
+        place: {
+          properties: {
+            location: {
+              type: "geo_shape",
+              precision: "10m"
+            }
           }
         }
       }
     }
-  }
+  end
 
   after_save do
     $es.index index: "shizuka", type: "place", id: id, body: {
       location: {
         type: "point",
-        coordinates: [latitude, longitude],
-        comments: comments.collect { |c|
-          { id: c.id, mood: c.mood, things: c.things }
-        }
+        coordinates: [latitude, longitude]
+      },
+      comments: comments.collect { |c|
+        { id: c.id, mood: c.mood, things: c.things }
       }
     }
   end
