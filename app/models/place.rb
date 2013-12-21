5023dd2f3911ade6193782b523e27a5bee7c7ac9
Li Donghua <donghua.li@dianping.com>
2013-12-21 08:43:36 +0800

    ElasticSearch bootstrap move to initializer.

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,21 +2,6 @@ class Place < ActiveRecord::Base
   validates :latitude, :longitude, :comments, presence: true
   has_many :comments
 
-  unless $es.indices.exists index: "shizuka"
-    $es.indices.create index: "shizuka", body: {
-      mappings: {
-        place: {
-          properties: {
-            location: {
-              type: "geo_shape",
-              precision: "10m"
-            }
-          }
-        }
-      }
-    }
-  end
-
   after_save do
     $es.index index: "shizuka", type: "place", id: id, body: {
       location: {
